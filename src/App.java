0021d8e9d4f6b409877591eaafc3f6d3f562c148
Juan <juandajimenezcriollo@gmail.com>
2025-07-15 19:36:10 -0500

    Trabajo

---
+++ src/App.java
@@ -0,0 +1,28 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        runEjercciosPD();
+    
+    }
+
+    private static void runEjercciosPD() {
+        EjerciciosPD ejerciciosPD = new EjerciciosPD();
+        System.out.println("Fibonaci Recursivo");
+        long start = System.nanoTime();
+        long resultado = ejerciciosPD.getFibonacci(5);
+        long end = System.nanoTime();
+        long duration = end - start;
+        System.out.println("Resultado = " + resultado + " en tiempo = " + duration);
+
+
+        System.out.println("Fibonaci Recursivo Caching ");
+        start = System.nanoTime();
+        resultado = ejerciciosPD.getFibonacci(100);
+        end = System.nanoTime();
+        duration = end - start;
+        System.out.println("Resultado = " + resultado + " en tiempo = " + duration);
+
+
+    }
+}
---
+++ src/EjerciciosPD.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+
+public class EjerciciosPD {
+    public long getFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
+
+    public long getFibonacciPD(int n) {
+        Map<Integer, Long> memo = new java.util.HashMap<>();
+        return getFibonacciPDHelper(n, memo);
+    }
+
+    private long getFibonacciPDHelper(int n, Map<Integer, Long> memo) {
+        if (n <= 1) {
+            return n;
+        }
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        long result = getFibonacciPDHelper(n - 1, memo) + getFibonacciPDHelper(n - 2, memo);
+        memo.put(n, result);
+        return result;
+    }
+}
