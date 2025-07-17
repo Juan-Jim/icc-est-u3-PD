b30bc2848ee07ba716c640679e5c4d3219b7ba3c
Juan <juandajimenezcriollo@gmail.com>
2025-07-16 21:38:35 -0500

    Trabajo

--- src/App.java
+++ src/App.java
@@ -1,28 +1,54 @@
 
+import java.util.Arrays;
+import java.util.List;
+
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        runEjercciosPD();
-    
+        //runEjercciosPD();
+        runMaze();
     }
 
-    private static void runEjercciosPD() {
-        EjerciciosPD ejerciciosPD = new EjerciciosPD();
-        System.out.println("Fibonaci Recursivo");
-        long start = System.nanoTime();
-        long resultado = ejerciciosPD.getFibonacci(50);
-        long end = System.nanoTime();
-        long duration = end - start;
-        System.out.println("Resultado = " + resultado + " en tiempo = " + duration);
+    //private static void runEjercciosPD() {
+        // EjerciciosPD ejerciciosPD = new EjerciciosPD();
+        // System.out.println("Fibonaci Recursivo");
+        // long start = System.nanoTime();
+        // long resultado = ejerciciosPD.getFibonacci(50);
+        // long end = System.nanoTime();
+        // long duration = end - start;
+        // System.out.println("Resultado = " + resultado + " en tiempo = " + duration);
 
 
-        System.out.println("Fibonaci Recursivo Caching ");
-        start = System.nanoTime();
-        resultado = ejerciciosPD.getFibonacciPD(50);
-        end = System.nanoTime();
-        duration = end - start;
-        System.out.println("Resultado = " + resultado + " en tiempo = " + duration);
+        // System.out.println("Fibonaci Recursivo Caching ");
+        // start = System.nanoTime();
+        // resultado = ejerciciosPD.getFibonacciPD(50);
+        // end = System.nanoTime();
+        // duration = end - start;
+        // System.out.println("Resultado = " + resultado + " en tiempo = " + duration);
 
+    private static void runMaze() { 
+        boolean[][] predefinedMaze = {
+            {true, true, true, true},
+            {false, true, true, true},
+            {true, true, false, false},
+            {true, true, true, true},
+        };
 
+        Maze maze = new Maze(predefinedMaze);
+        System.out.println("Laberinto cargado");
+        maze.printMaze();
+
+        Cell start = new Cell(0, 0);
+        Cell end = new Cell(3, 3);
+
+         List<MazeSolver.MazeSolverStrategy> solvers = Arrays.asList(
+            new MazeSolverRecursivo()
+        );
+
+        MazeSolver.MazeSolverStrategy solver = solvers.get(0);
+        List<Cell> path = solver.getPath(predefinedMaze, start, end);
+        System.out.println("Camino encontrado:");
+        System.out.println(path);
     }
 }
---
+++ src/MazeSolver.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class MazeSolver {
+    public interface MazeSolverStrategy {
+        List<Cell> getPath(boolean[][] grid, Cell start, Cell end);
+    }
+}
